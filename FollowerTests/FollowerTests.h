276b48488b89a9238f4ea63479b61cfbb98c9707
liping Yin <81168900@qq.com>
2013-08-14 08:52:05 +0800

    Initial Commit

---
+++ FollowerTests/FollowerTests.h
@@ -0,0 +1,13 @@
+//
+//  FollowerTests.h
+//  FollowerTests
+//
+//  Created by Liping Yin on 13-8-14.
+//  Copyright (c) 2013年 Liping Yin. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface FollowerTests : SenTestCase
+
+@end
---
+++ FollowerTests/FollowerTests.m
@@ -0,0 +1,32 @@
+//
+//  FollowerTests.m
+//  FollowerTests
+//
+//  Created by Liping Yin on 13-8-14.
+//  Copyright (c) 2013年 Liping Yin. All rights reserved.
+//
+
+#import "FollowerTests.h"
+
+@implementation FollowerTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in FollowerTests");
+}
+
+@end
